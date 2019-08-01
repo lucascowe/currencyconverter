68dad92821ddc38b58ff5e580fcf0d2900f26797
lucas <lucascowe@gmail.com>
2019-07-31 23:38:10 -0600

    Saving work

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,12 +15,23 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import static com.example.currencyconverter.R.drawable.aus;
-import static com.example.currencyconverter.R.drawable.can;
-import static com.example.currencyconverter.R.drawable.eng;
-import static com.example.currencyconverter.R.drawable.usa;
-
 public class MainActivity extends AppCompatActivity {
+
+    public class Currency {
+        public String country;
+        public double value;
+        public int image;
+        public int pos;
+
+
+        public Currency(String c, double v, int i, int p) {
+            Log.i("Info","New Currency added " + c + " worth $" + v);
+            country = c;
+            value = v;
+            image = i;
+            pos = p;
+        }
+    }
 
     RadioGroup radGroup;
     RadioButton radBut;
@@ -31,34 +43,28 @@ public class MainActivity extends AppCompatActivity {
     EditText yourMoney;
     double mainRate;
 
-    Currency[] currency;
+    Currency[] currency = new Currency[4];
 
-    public class Currency {
-        public String[] country;
-        public double value;
-        int image;
-        int pos;
-
-
-        public Currency(String country, double value, int image, int pos) {
-        }
-    }
-
-    public void changeCurrency (View view) {
+    public void convert (View view) {
         // get selected button
         int selectedCurrency = radGroup.getCheckedRadioButtonId();
-        radBut = (RadioButton) findViewById(selectedCurrency);
+        radBut = findViewById(selectedCurrency);
         // match with currency
         int others = 1;
-        for (int ii = 0; ii < currency.length; ii++) {
-            if (radBut.getText().toString() == currency[ii].country.toString()) {
+        for (int ii = 0; ii < 4; ii++) {
+            Log.i("Check","radBut " + radBut.getText() + " vs " + currency[ii].country);
+            if (radBut.getText().toString().equals(currency[ii].country)) {
+                Log.i("Progress", "Main country " + currency[ii].country);
                 currency[ii].pos = 0;
-                mainIV.setImageResource(currency[ii].image);
+                ratePics[0].setImageResource(currency[ii].image);
+                Log.i("Progress", "Main image is  " + currency[ii].image);
                 mainRate = currency[ii].value;
+                Log.i("Progress", "Main rate is  " + mainRate);
             }
             else {
                 currency[ii].pos = others;
-                ratePics[others - 1].setImageResource(currency[ii].image);
+                Log.i("Other", "Pos " + currency[ii].pos + " Country " +currency[ii].country + " image " + currency[ii].image);
+                ratePics[others].setImageResource(currency[ii].image);
                 others++;
             }
         }
@@ -67,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         for (int ii = 0; ii < currency.length; ii++) {
             if (currency[ii].pos > 0) {
                 temp = dblYouMoney / mainRate * currency[ii].value;
-                c1.setText(String.valueOf(temp));
+                theRates[currency[ii].pos-1].setText(String.valueOf(temp));
             }
         }
 
@@ -77,25 +83,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        currency = new Currency[4];
-        currency[0] = new Currency("AUS",0.7, aus, 0);
-        currency[1] = new Currency("USA",1,usa, 1);
-        currency[2] = new Currency("CAN",0.8,can, 2);
-        currency[3] = new Currency("GBP",1.4, eng, 3);
+        currency[0] = new Currency("AUS",0.7, R.drawable.aus, 0);
+        Log.i("Info", "currency[0] created for " + currency[0].country);
+        currency[1] = new Currency("USA",1, R.drawable.usa, 1);
+        currency[2] = new Currency("CAN",0.8, R.drawable.can, 2);
+        currency[3] = new Currency("ENG",1.4, R.drawable.eng, 3);
         radGroup = findViewById(R.id.radGrp);
-        mainIV = findViewById(R.id.mainImageView);
-        c1IV = findViewById(R.id.c1ImageView);
-        c2IV = findViewById(R.id.c2ImageView);
-        c3IV = findViewById(R.id.c3ImageView);
-        c1 = findViewById(R.id.c1TextView);
-        c2 = findViewById(R.id.c2TextView);
-        c3 = findViewById(R.id.c3TextView);
+        convertButton = findViewById(R.id.convertBnt);
+        ratePics[0] = findViewById(R.id.mainImageView);
+        ratePics[1] = findViewById(R.id.c1ImageView);
+        ratePics[2] = findViewById(R.id.c2ImageView);
+        ratePics[3] = findViewById(R.id.c3ImageView);
+        theRates[0] = findViewById(R.id.c1TextView);
+        theRates[1] = findViewById(R.id.c2TextView);
+        theRates[2] = findViewById(R.id.c3TextView);
         yourMoney = findViewById(R.id.youMoneyEditText);
 
         convertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                changeCurrency(view);
+                convert(view);
             }
         });
     }
